c66c40b5791da24991a9da6234e9fe5ab801f435
Rick <HengYi_lin@htc.com>
2015-03-08 21:06:43 +0800

    Add a prefix for routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :users, except: [:show]
-  root "home#index"
+  scope :wheatbeer do
+    resources :users, except: [:show]
+    root "home#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -56,4 +57,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  end
 end
