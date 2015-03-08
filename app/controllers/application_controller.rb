713c807add5728bd97ffc93f33f20f48ade2fe74
Rick <HengYi_lin@htc.com>
2015-03-08 21:28:58 +0800

    Catch all exceptions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  around_filter :catch_exceptions
+
+  def catch_exceptions
+    begin
+      yield
+    rescue => e
+      Rails.logger.error e
+      redirect_to root_path, alert: e.message
+    end
+  end
 end
